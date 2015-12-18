8f3f129275bd5194b9c37f8c717e1cf2a83afbee
CenChao <534815812@qq.com>
2015-12-18 13:13:32 +0800

    Initial Commit

---
+++ BigHomework/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BigHomework
+//
+//  Created by noosc on 15/12/18.
+//  Copyright __MyCompanyName__ 2015年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
